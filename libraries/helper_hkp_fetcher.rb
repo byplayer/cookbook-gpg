968bf864264d0d5819d4a240b022c1a77f2fa78f
Yukio Goto <yukio.goto@rakuten.com>
2015-12-16 14:22:33 +0800

    add retry to get key

--- libraries/helper_hkp_fetcher.rb
+++ libraries/helper_hkp_fetcher.rb
@@ -7,6 +7,9 @@ module BswTech
         parsed = URI.parse key_server
         url_with_port = parsed.scheme ? key_server : "http://#{key_server}"
         full_url = "#{url_with_port}/pks/lookup?options=mr&op=get&search=0x#{URI.escape key_id}"
+        # retry 10 times, because http isn't stable
+        max_retry = 10
+        try_count = 0
         begin
           open(full_url) do |f|
             raw = f.read
@@ -14,6 +17,10 @@ module BswTech
             match[0]
           end
         rescue OpenURI::HTTPError => e
+          if try_count <= max_retry
+            try_count += 1
+            retry
+          end
           raise "Contacted key server OK, but key ID '#{key_id}' was not found: #{e}"
         rescue SocketError => e
           raise "Unable to contact key server '#{full_url}', details: #{e}"
