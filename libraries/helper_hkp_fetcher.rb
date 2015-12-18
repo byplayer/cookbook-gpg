ed66eb5d5bb22affb8daaa0f8ffd9f6beceb89d6
Yukio Goto <yukio.goto@rakuten.com>
2015-12-18 10:32:03 +0800

    add log

--- libraries/helper_hkp_fetcher.rb
+++ libraries/helper_hkp_fetcher.rb
@@ -8,7 +8,7 @@ module BswTech
         url_with_port = parsed.scheme ? key_server : "http://#{key_server}"
         full_url = "#{url_with_port}/pks/lookup?options=mr&op=get&search=0x#{URI.escape key_id}"
         # retry 10 times, because http isn't stable
-        max_retry = 10
+        max_retry = 50
         try_count = 0
         begin
           open(full_url) do |f|
@@ -19,6 +19,8 @@ module BswTech
         rescue OpenURI::HTTPError => e
           if try_count <= max_retry
             try_count += 1
+            Chef::Log.logger.info "key server return error. retry(#{try_count})"
+            sleep 1
             retry
           end
           raise "Contacted key server OK, but key ID '#{key_id}' was not found: #{e}"
